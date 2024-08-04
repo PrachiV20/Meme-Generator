c8a246f5cee90fbc1bad82b09f82170fad4426ba
PrachiV20 <vermaprachi202@gmail.com>
2024-08-04 15:56:43 +0530

    Random meme generator

---
+++ script.js
@@ -0,0 +1,69 @@
+const generateMemeBtn = document.querySelector(
+    ".meme-generator .generate-meme-btn "
+  );
+  
+  
+  const memeImage = document.querySelector(".meme-generator img");
+  const memeTitle = document.querySelector(".meme-generator .meme-title");
+
+  
+  const updateDetails = (url, title) => {
+    memeImage.setAttribute("src", url);
+    memeTitle.innerHTML = title;
+    
+  };
+  
+  const generateMeme = () => {
+    fetch("https://meme-api.com/gimme/wholesomememes")
+      .then((response) => response.json())
+      .then((data) => {
+        updateDetails(data.url, data.title);
+      });
+  };
+  
+  generateMemeBtn.addEventListener("click", generateMeme);
+  
+  generateMeme();
+  
+// EMOJI 
+  const btn = document.querySelector("#emoji");
+const emojis = [
+    "😆",
+    "😅",
+    "🤣",
+    "😂",
+    "😀",
+    "🤑",
+    "🤨",
+    "🙂",
+    "😊",
+    "😗",
+    "😛",
+    "😏",
+    "🤥",
+    "😴",
+    "🥺",
+    "😧",
+    "😇",
+    "😳",
+    "🙃",
+    "🥴",
+    "🧐",
+    "🤨",
+    "😒",
+    "🤔",
+    "🤭",
+    "🥰",
+    "🤐",
+    "👀",
+    "🤔",
+    "🤪",
+    "🥲",
+    "😃",
+    "😁",
+    "😬",
+];
+
+btn.addEventListener("mouseover", () => {
+    btn.innerHTML = emojis[Math.floor(Math.random() * emojis.length)];
+})
